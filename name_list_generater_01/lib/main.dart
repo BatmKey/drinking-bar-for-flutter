e91d2399412c15ae1a4272b80b5423f4793dbf83
HenryWang <wyh_power@126.com>
2021-06-04 23:19:47 +0800

    001:step2-> Use an External Package

--- name_list_generater_01/lib/main.dart
+++ name_list_generater_01/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordpair = WordPair.random();
     return MaterialApp(
         title: "this is my first to flutter",
         home: Scaffold(
           appBar: AppBar(title: Text('App bar')),
           body: Center(
-            child: Text("Hello world"),
+            child: Text(wordpair.asPascalCase)
           ),
         ));
   }
